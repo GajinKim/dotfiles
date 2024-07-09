d98ae81c1c0535db2e29750ee26215e5f6583b3a
Gajin Kim <gajin2000@gmail.com>
2024-07-09 10:06:03 -0500

    update

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -2,6 +2,12 @@
 
 echo -n "Configuring ZSH... "
 cp -r .zsh ~
+
 echo "PATH=$PATH" >> ~/.zsh/paths
 cp .zshrc ~
+
+echo "\nRunning source ZSH"
 source ~/.zshrc
+refresh # in case source ~/.zshrc doesn't work
+
+
