d6325cf1ad42a4f6a1f3b84c5d002f0608741d96
Gajin Kim <gajin2000@gmail.com>
2024-07-19 10:51:47 -0500

    removed unnecessary nested lua directory

--- .config/nvim/lua/lua/init.lua
+++
@@ -1,7 +0,0 @@
--- Load all lua scripts in this folder
-require('plugins') -- Should be loaded first
-require('appearance')
-require('plugin-settings')
-require('lsp')
-require('autocomplete')
-require('indent-blankline')
